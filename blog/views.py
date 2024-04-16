763cc993f3b8cc9663f2ef2b659a85bf52e4e8cf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-04-16 10:47:55 +0000

    did my first routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    # Return this following response when the called.
+    return HttpResponse('<h1>Blog Home!</h1>')
